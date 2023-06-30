68605685caa8932c54fe9faa1e96d6aa351af3fc
Iryna Laktionova <iryna.laktionova@nixsolutions.com>
2023-06-30 22:24:20 +0300

    lesson-work

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './sass/main.scss';
+import robot from './assets/robot.png';
+
+const mainImage = document.getElementById('mainImage');
+mainImage.src = robot;
+
+const hello = require('./js/hello');
